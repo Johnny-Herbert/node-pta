8bfc1070854dcf6e5f68a5cc2bcaa7ec442fe55a
Johnny Herbert Muniz Nunes <jhmn@ug2c18.windows.cin.ufpe.br>
2019-06-01 16:07:51 -0300

    Aula node

---
+++ app.js
@@ -0,0 +1,12 @@
+const app = require('./config/server')
+
+require('./app/routes/home')(app);
+
+require('./app/routes/contato')(app);
+
+// require('.app/routes/notFound')(app);
+
+
+app.listen(3000, () => {
+    console.log("Listenidsadsadsad");
+});
